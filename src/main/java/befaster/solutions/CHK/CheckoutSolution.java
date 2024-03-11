a1438799d09ec63f6cea24697a46d4aa5d2f0779
sbx_user1051 <sbx_user1051@169.254.15.109>
2024-03-11 15:42:00 +0000

    Mon Mar 11 15:42:00 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -11,15 +11,26 @@ public class CheckoutSolution {
 
     private int calculateStringValue(String str){
         int totalValue = 0;
+        int countB =0;
+        int countA = 0;
 
         for(int i=0; i<str.length(); i++){
             char letter = str.charAt(i);
             switch(letter){
                 case 'A':
                     totalValue+=50;
+                    countA++;
+                    if(countA==3){
+                        countA=0;
+                        totalValue -=20;
+                    }
                     break;
                 case'B':
                     totalValue+=30;
+                    if(countB==2){
+                        countB=0;
+                        totalValue -=15;
+                    }
                     break;
                 case 'C':
                     totalValue+=20;
@@ -35,6 +46,7 @@ public class CheckoutSolution {
         return totalValue;
     }
 }
+
 
 
 
