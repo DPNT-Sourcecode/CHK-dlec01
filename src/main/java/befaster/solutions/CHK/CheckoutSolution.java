ebcf46115b86e6ccd53c773bc7c3e329888ab3d1
sbx_user1051 <sbx_user1051@169.254.15.109>
2024-03-11 16:34:08 +0000

    Mon Mar 11 16:34:08 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -13,6 +13,7 @@ public class CheckoutSolution {
         int totalValue = 0;
         int countB =0;
         int countA = 0;
+        int countE= 0;
 
         for(int i=0; i<str.length(); i++){
             char letter = str.charAt(i);
@@ -20,18 +21,18 @@ public class CheckoutSolution {
                 case 'A':
                     totalValue+=50;
                     countA++;
-                    if(countA==3){
-                        countA=0;
-                        totalValue -=20;
-                    }
+//                    if(countA==3){
+//                        countA=0;
+//                        totalValue -=20;
+//                    }
                     break;
                 case'B':
                     totalValue+=30;
                     countB++;
-                    if(countB==2){
-                        countB=0;
-                        totalValue -=15;
-                    }
+//                    if(countB==2){
+//                        countB=0;
+//                        totalValue -=15;
+//                    }
                     break;
                 case 'C':
                     totalValue+=20;
@@ -39,11 +40,47 @@ public class CheckoutSolution {
                 case 'D':
                     totalValue+=15;
                     break;
+                case'E':
+                    countE++;
+                    totalValue+=40;
+                    break;
                 default:
                     return -1;
 
             }
         }
-        return totalValue;
+         int aDiscount =calulateDiscounts(countA);
+        int bNEdisocunt =eBPromotion(countE,countB);
+        return totalValue -aDiscount-bNEdisocunt;
+    }
+
+    private int eBPromotion(int countE, int countB){
+        int countOfFreeB =countE/2;
+        while(countB>=1){
+            if(countOfFreeB>0){
+                countB--;
+                countOfFreeB--;
+            }
+        }
+
+        int evenB = countB/2;
+        return evenB*15;
+
+    }
+
+    private int calulateDiscounts(int countA){
+        int totalDiscountValue=0;
+        while(countA>=5){
+            totalDiscountValue+=50;
+            countA-=5;
+        }
+        while (countA>=3){
+            totalDiscountValue+=20;
+            countA-=3;
+        }
+
+        return totalDiscountValue;
+
     }
 }
+
